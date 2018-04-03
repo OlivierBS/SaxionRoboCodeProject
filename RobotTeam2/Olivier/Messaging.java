ad3a1cd98aba78ed68bfebe22eb5ba5a857c99a6
OlivierBS <37328479+OlivierBS@users.noreply.github.com>
2018-04-03 14:56:43 +0200

    Create Messaging.java

---
+++ RobotTeam2/Olivier/Messaging.java
@@ -0,0 +1,28 @@
+package nl.saxion;
+
+import robocode.TeamRobot;
+
+public class Messaging {
+
+    private TeamRobot robot;
+    private Utilities utilities;
+
+
+    public Messaging(TeamRobot robot, Utilities utilities){
+        this.robot = robot;
+        this.utilities = utilities;
+    }
+
+    public void messageBot(String name){
+
+        int idS = utilities.getID(name);
+
+        if (idS == 1) {
+            System.out.println("Corner Defender " + idS + " is dead!");
+        } else if (idS == 2) {
+            System.out.println("Corner Defender " + idS + " is dead!");
+        }
+    }
+
+
+}
