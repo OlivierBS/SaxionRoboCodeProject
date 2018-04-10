4f7f27effa1702f52e87d6c6a30106294a693275
OlivierBS <37328479+OlivierBS@users.noreply.github.com>
2018-04-10 14:04:43 +0200

    Update StrafeBot.java

--- RobotTeam2/StrafeBot.java
+++ RobotTeam2/StrafeBot.java
@@ -1,10 +1,8 @@
-package nl.saxion.Sprint2Team;
+package nl.saxion;
 
 import robocode.*;
-
 import java.awt.*;
 import java.io.IOException;
-
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class StrafeBot extends TeamRobot {
@@ -15,22 +13,52 @@ public class StrafeBot extends TeamRobot {
     private static final Color BULLET_COLOR = new Color(58, 2, 7);
     private static final Color SCAN_COLOR = new Color(237, 239, 255);
 
-    private Utilities utilities;
     private Movement movement;
 
     private String currentTarget = null;
     private boolean targetAssigned = false;
 
+    /**
+     * Standard run method, which gets called when robot spawns.
+     */
     public void run() {
-        utilities = new Utilities(this);
-        movement = new Movement(this, utilities);
-        utilities.setDefenderColor(BODY_COLOR, GUN_COLOR, RADAR_COLOR, BULLET_COLOR, SCAN_COLOR);
-        movement.goToPos();
+        Utilities.setRobotColor(this, BODY_COLOR, GUN_COLOR, RADAR_COLOR, BULLET_COLOR, SCAN_COLOR);
+
+        movement = new Movement(this);
+        movement.strafeBotGoToPos();
 
         while (true) {
             movement.strafeBotStrafing();
-            setTurnRadarRight(360); // Scan until you find your first enemy
+            setTurnRadarRight(360); // scan until you find your first enemy
             execute();
+        }
+    }
+
+    /**
+     * Lets the robot shoot at a target.
+     * @author Yao Lee, William
+     * @param event ScannedRobotEvent event.
+     */
+    public void firingMechanicStrafer(ScannedRobotEvent event) {
+        double absoluteBearing = getHeading() + event.getBearing();
+        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+        double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
+
+        if (Math.abs(bearingFromGun) <= 3) {
+            setTurnGunRight(bearingFromGun);
+            setTurnRadarRight(bearingFromRadar); // Keep the radar focused on the enemy
+
+            if (getGunHeat() == 0 && getEnergy() > .2) {
+                // Never fires a bullet with a power bigger than 3
+                // Fires a bullet with at least a power of 2
+                setFire(Math.min((400 / event.getDistance()) + 2, 3));
+            }
+        } else {
+            setTurnGunRight(bearingFromGun);
+            setTurnRadarRight(bearingFromRadar);
+        }
+        if (bearingFromGun == 0) {
+            scan();
         }
     }
 
@@ -41,12 +69,14 @@ public class StrafeBot extends TeamRobot {
      */
 
     /**
+     * onMessageReceived checks if the robot receives target and assigns it to the variable.
      * @author Mike, William, Olivier
+     * @param event MessageEvent event.
      */
     @Override
-    public void onMessageReceived(MessageEvent messageEvent) {
-        if (messageEvent.getMessage() instanceof String) {
-            String message = messageEvent.getMessage().toString();
+    public void onMessageReceived(MessageEvent event) {
+        if (event.getMessage() instanceof String) {
+            String message = event.getMessage().toString();
             if (message.contains("TARGET:")) {
                 targetAssigned = true;
                 currentTarget = message.substring(6);
@@ -56,7 +86,9 @@ public class StrafeBot extends TeamRobot {
     }
 
     /**
+     * onHitByBullet targets enemy robot that hit the robot.
      * @author Yao Lee
+     * @param event HitByBulletEvent event.
      */
     public void onHitByBullet(HitByBulletEvent event) {
         // calculate exact location of enemy
@@ -69,61 +101,37 @@ public class StrafeBot extends TeamRobot {
     }
 
     /**
+     * onScannedRobot assigns new target or targets target.
      * @author Mike, Olivier, William
+     * @param event ScannedRobotEvent event.
      */
-    public void onScannedRobot(ScannedRobotEvent e) {
-        if (!isTeammate(e.getName())) {
+    public void onScannedRobot(ScannedRobotEvent event) {
+        if (!isTeammate(event.getName())) {
             if (!targetAssigned) {
                 try {
-                    broadcastMessage("TARGET:" + e.getName());
-                    currentTarget = e.getName();
+                    broadcastMessage("TARGET:" + event.getName());
+                    currentTarget = event.getName();
                     targetAssigned = true;
                 } catch (IOException ioe) {
                     ioe.printStackTrace();
                 }
             }
-
             if (targetAssigned) {
-                if (e.getName().equals(currentTarget)) {
-                    firingMechanicStrafer(e);
+                if (event.getName().equals(currentTarget)) {
+                    firingMechanicStrafer(event);
                 } else {
                     scan();
                 }
             } else {
-                firingMechanicStrafer(e);
+                firingMechanicStrafer(event);
             }
         }
     }
 
     /**
-     * @author Yao Lee, William
-     */
-    public void firingMechanicStrafer(ScannedRobotEvent e) {
-        double absoluteBearing = getHeading() + e.getBearing();
-        double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-        double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
-
-        if (Math.abs(bearingFromGun) <= 3) {
-            setTurnGunRight(bearingFromGun);
-            setTurnRadarRight(bearingFromRadar); // Keep the radar focused on the enemy
-
-            if (getGunHeat() == 0 && getEnergy() > .2) {
-                // Never fires a bullet with a power bigger than 3
-                // Fires a bullet with at least a power of 2
-                setFire(Math.min((400 / e.getDistance()) + 2, 3));
-            }
-        } else {
-            setTurnGunRight(bearingFromGun);
-            setTurnRadarRight(bearingFromRadar);
-        }
-
-        if (bearingFromGun == 0) {
-            scan();
-        }
-    }
-
-    /**
+     * onRobotDeath which checks if the target died.
      * @author Mike, William
+     * @param event RobotDeathEvent event.
      */
     @Override
     public void onRobotDeath(RobotDeathEvent event) {
