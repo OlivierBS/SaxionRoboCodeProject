7783eb13f3c92fac1de4ee7605ecd0fa121018c8
William <37328509+WGTW@users.noreply.github.com>
2018-04-10 12:47:11 +0200

    Major update to StrafeBot class
    Added several new methods, fixes and included all @authors.

--- RobotTeam2/StrafeBot.java
+++ RobotTeam2/StrafeBot.java
@@ -1,50 +1,37 @@
-package nl.saxion;
+package nl.saxion.Sprint2Team;
 
-import robocode.MessageEvent;
-import robocode.TeamRobot;
+import robocode.*;
 
 import java.awt.*;
+import java.io.IOException;
+
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 public class StrafeBot extends TeamRobot {
 
+    private static final Color BODY_COLOR = new Color(143, 161, 255);
+    private static final Color GUN_COLOR = new Color(14, 0, 57);
+    private static final Color RADAR_COLOR = new Color(205, 255, 199);
+    private static final Color BULLET_COLOR = new Color(58, 2, 7);
+    private static final Color SCAN_COLOR = new Color(237, 239, 255);
+
     private Utilities utilities;
-    private Messaging messaging;
     private Movement movement;
 
+    private String currentTarget = null;
+    private boolean targetAssigned = false;
 
     public void run() {
-
-        setDefenderColor();
-
         utilities = new Utilities(this);
-        messaging = new Messaging(this,utilities);
-        movement = new Movement(this,utilities,messaging);
-
+        movement = new Movement(this, utilities);
+        utilities.setDefenderColor(BODY_COLOR, GUN_COLOR, RADAR_COLOR, BULLET_COLOR, SCAN_COLOR);
         movement.goToPos();
 
         while (true) {
             movement.strafeBotStrafing();
-            setTurnRadarRight(360); // scan until you find your first enemy
+            setTurnRadarRight(360); // Scan until you find your first enemy
             execute();
         }
-
-    }
-
-    /*
-                               +--------------------------------+
-            +------------------+        Robot Utilities         +------------------+
-                               +--------------------------------+
-     */
-
-    /**
-     * @author William
-     */
-    private void setDefenderColor() {
-        setBodyColor(new Color(0, 0, 0));
-        setGunColor(new Color(255, 0, 0));
-        setRadarColor(new Color(0, 0, 0));
-        setBulletColor(new Color(255, 0, 0));
-        setScanColor(new Color(0, 0, 0));
     }
 
     /*
@@ -53,17 +40,25 @@ public class StrafeBot extends TeamRobot {
                                +--------------------------------+
      */
 
+    /**
+     * @author Mike, William, Olivier
+     */
     @Override
-    public void onMessageReceived(MessageEvent event) {
-        super.onMessageReceived(event);
-        if(event.getMessage() instanceof String) {
-            String message = (String) event.getMessage();
+    public void onMessageReceived(MessageEvent messageEvent) {
+        if (messageEvent.getMessage() instanceof String) {
+            String message = messageEvent.getMessage().toString();
+            if (message.contains("TARGET:")) {
+                targetAssigned = true;
+                currentTarget = message.substring(6);
+            }
+
         }
     }
-    
-     public void onHitByBullet(HitByBulletEvent event){
-        System.out.println(event.getName() + " hit me!");
 
+    /**
+     * @author Yao Lee
+     */
+    public void onHitByBullet(HitByBulletEvent event) {
         // calculate exact location of enemy
         double absoluteBearing = getHeading() + event.getBearing();
         double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
@@ -73,19 +68,49 @@ public class StrafeBot extends TeamRobot {
         setTurnRadarRight(bearingFromRadar);
     }
 
+    /**
+     * @author Mike, Olivier, William
+     */
     public void onScannedRobot(ScannedRobotEvent e) {
+        if (!isTeammate(e.getName())) {
+            if (!targetAssigned) {
+                try {
+                    broadcastMessage("TARGET:" + e.getName());
+                    currentTarget = e.getName();
+                    targetAssigned = true;
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                }
+            }
+
+            if (targetAssigned) {
+                if (e.getName().equals(currentTarget)) {
+                    firingMechanicStrafer(e);
+                } else {
+                    scan();
+                }
+            } else {
+                firingMechanicStrafer(e);
+            }
+        }
+    }
+
+    /**
+     * @author Yao Lee, William
+     */
+    public void firingMechanicStrafer(ScannedRobotEvent e) {
         double absoluteBearing = getHeading() + e.getBearing();
         double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
         double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
 
         if (Math.abs(bearingFromGun) <= 3) {
             setTurnGunRight(bearingFromGun);
-            setTurnRadarRight(bearingFromRadar); // keep the radar focused on the enemy
+            setTurnRadarRight(bearingFromRadar); // Keep the radar focused on the enemy
 
             if (getGunHeat() == 0 && getEnergy() > .2) {
-                // never fires a bullet bigger than 3, just flooring the value to be safe
-                // fires at least a bullet of 2
-                setFire(Math.min((400 / e.getDistance()) + 2 , 3));
+                // Never fires a bullet with a power bigger than 3
+                // Fires a bullet with at least a power of 2
+                setFire(Math.min((400 / e.getDistance()) + 2, 3));
             }
         } else {
             setTurnGunRight(bearingFromGun);
@@ -94,6 +119,16 @@ public class StrafeBot extends TeamRobot {
 
         if (bearingFromGun == 0) {
             scan();
+        }
+    }
+
+    /**
+     * @author Mike, William
+     */
+    @Override
+    public void onRobotDeath(RobotDeathEvent event) {
+        if (currentTarget.equals(event.getName())) {
+            targetAssigned = false;
         }
     }
 }
